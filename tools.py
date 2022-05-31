2c15b77d6019327d752357377d39c8b43c376a45
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2022-05-31 11:17:06 -0700

    fixed issue with pixel coordinate centering

--- tools.py
+++ tools.py
@@ -30,6 +30,18 @@ def weightmean(vals, rmss, axis=None):
     meanrms = np.sqrt(1/np.nansum(1/rmss**2, axis=axis))
     return meanval, meanrms
 
+def freq_to_z(nuem, nuobs):
+    zval = (nuem - nuobs) / nuobs
+    return zval
+
+def nuem_to_nuobs(nuem, z):
+    nuobs = nuem / (1 + z)
+    return nuobs
+
+def nuobs_to_nuem(nuobs, z):
+    nuem = nuobs * (1 + z)
+    return nuem
+
 def coord_to_pix(coords, comap):
     """
     given a coordinate value in degrees, return the (x,y) coordinates
@@ -41,7 +53,8 @@ def coord_to_pix(coords, comap):
 
 def load_map(file, reshape=True):
     """
-    loads in a file in the COMAP format, storing everything as arrays in the map class
+    loads in a file in the COMAP format, storing everything as arrays in the map class.
+    COMAP data are stored with coordinates as the CENTER of each pixel
     """
     # *** give maps their own special class at some point?
 
@@ -57,11 +70,7 @@ def load_map(file, reshape=True):
         patch_cent = np.array(mapfile.get('patch_center'))
         comap.fieldcent = SkyCoord(patch_cent[0]*u.deg, patch_cent[1]*u.deg)
 
-    # housekeeping for the arrays - give each axis an index array as well
-    comap.x = np.arange(len(comap.ra))
-    comap.y = np.arange(len(comap.dec))
-
-    # mark pixels with zero rms and mask them in the rms/map arrays (how the pipeline stores infs)
+        # mark pixels with zero rms and mask them in the rms/map arrays (how the pipeline stores infs)
     comap.badpix = np.where(rmstemparr < 1e-10)
     maptemparr[comap.badpix] = np.nan
     rmstemparr[comap.badpix] = np.nan
@@ -80,10 +89,22 @@ def load_map(file, reshape=True):
     comap.xstep = (comap.ra[1] - comap.ra[0])/2
     comap.ystep = (comap.dec[1] - comap.dec[0])/2
 
+    # housekeeping for the arrays - give each axis an index array as well
+    comap.x = np.arange(len(comap.ra))
+    comap.y = np.arange(len(comap.dec))
+
+    # rearrange so that the stored coordinate coordinate arrays correspond to the
+    # bottom right (etc.) of the voxel (currently they're the center)
+    comap.freq = comap.freq - comap.fstep / 2
+    comap.ra = comap.ra - comap.xstep / 2
+    comap.dec = comap.dec - comap.ystep / 2
+
+
     # limits on each axis for easy testing
     comap.flims = (np.min(comap.freq), np.max(comap.freq))
     comap.xlims = (np.min(comap.ra), np.max(comap.ra))
     comap.ylims = (np.min(comap.dec), np.max(comap.dec))
+
     # *** any other per-field info we need
 
     return comap
@@ -92,6 +113,11 @@ def setup(mapfiles, cataloguefile, params):
     maplist = []
     for mapfile in mapfiles:
         mapinst = load_map(mapfile)
+
+        # calculate the appropriate redshift limits from the freq axis
+        zlims = freq_to_z(params.centfreq, np.array(mapinst.flims))
+        mapinst.zlims = np.sort(zlims)
+
         maplist.append(mapinst)
 
     catdict = {}
@@ -102,18 +128,20 @@ def setup(mapfiles, cataloguefile, params):
 
     catlist = []
     for i in range(len(mapfiles)):
-        catinst = field_cull_galaxy_cat(catdict, maplist[i].fieldcent)
+        catinst = field_cull_galaxy_cat(catdict, maplist[i])
         catlist.append(catinst)
 
     return maplist, catlist
 
-def field_cull_galaxy_cat(galdict, fieldcent, maxsep=3*u.deg):
+def field_cull_galaxy_cat(galdict, comap, maxsep=3*u.deg):
     """
     takes the full version of the catalogue to be stacked on and cuts to all objects within some
     radius of the given field center
     """
     # *** get rid of skycoord dependence
     # allow you to carry around other arbitrary parameters? ****
+    fieldcent = comap.fieldcent
+    zlims = np.array(comap.zlims)
 
     # pull only objects in the field
     fieldcoords = SkyCoord(galdict['ra']*u.deg, galdict['dec']*u.deg)
@@ -121,7 +149,7 @@ def field_cull_galaxy_cat(galdict, fieldcent, maxsep=3*u.deg):
     fieldidx = np.where(fieldsep < maxsep)[0]
 
     fieldz_cut = galdict['z'][fieldidx]
-    fieldidx = fieldidx[np.where(np.logical_and(fieldz_cut > 2.35, fieldz_cut < 3.45))[0]]
+    fieldidx = fieldidx[np.where(np.logical_and(fieldz_cut > zlims[0], fieldz_cut < zlims[1]))[0]]
 
     # save to cat object
     galcat = empty_table()
