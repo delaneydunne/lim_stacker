520645a8743c01fc7f49625a5ab23cc191442515
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2022-06-04 18:44:09 -0700

    simulation functions

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 from   .tools               import *
 from   .stack               import *
 from   .bootstrap           import *
+from   .siminject           import *
