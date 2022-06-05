328b15da2d175a305d3e9698c25b34a0743fe055
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2022-06-04 18:41:26 -0700

    simulation functions

---
+++ siminject.py
@@ -0,0 +1,122 @@
+from __future__ import absolute_import, print_function
+from .tools import *
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+import astropy.constants as const
+import os
+import pandas as pd
+import h5py
+import glob
+from astropy.convolution import convolve, Gaussian2DKernel
+from astropy.io import fits
+
+
+""" COMBINE ALREADY-MADE SIMS WITH DATA """
+def load_sim(file):
+    """
+    loads in a mock CO luminosity cube and stores as an object like the real
+    map
+    """
+
+    simmap = empty_table()
+
+    with np.load(file) as simfile:
+        # sims output uK, data in K. stack functions all deal w K so convert
+        simmap.rawmap = simfile['map_cube'].T / 1e6
+        simmap.freq = simfile['map_frequencies']
+        simmap.ra = simfile['map_pixel_ra']
+        simmap.dec = simfile['map_pixel_dec']
+
+    # **anything else here?
+
+    return simmap
+
+def beam_smooth_sim(simmap, fwhm=4.5):
+    """
+    Smooth the simulated data with a Gaussian 2D kernel to approximate the wider COMAP beam
+    fwhm should be the beam fwhm in arcmin
+    """
+
+    std = fwhm / (2*np.sqrt(2*np.log(2)))
+    pixwidth = (simmap.ra[1] - simmap.ra[0])*60
+
+    std_pix = std / pixwidth
+
+    beamkernel = Gaussian2DKernel(std_pix)
+
+    # loop over the frequency axis and convolve each frame
+    smoothsimlist = []
+    for i in range(len(simmap.freq)):
+        smoothsimlist.append(convolve(simmap.rawmap[i,:,:], beamkernel))
+
+    simmap.map = np.stack(smoothsimlist, axis=0)
+
+    return simmap
+
+def dump_map(comap, filename):
+    """
+    save a map class as a hdf5 file
+    this will be more bare-bones than the actual datafiles -- just including the
+    datasets needed for stacking
+    """
+
+    # *** automatic output file naming
+
+    with h5py.File(filename, 'w') as f:
+        dset = f.create_dataset('map_coadd', data = comap.simdatmap, dtype='float64')
+        dset = f.create_dataset('rms_coadd', data = comap.rms, dtype='float64')
+        dset = f.create_dataset('freq', data = comap.freq, dtype='float64')
+        dset = f.create_dataset('x', data = comap.ra, dtype='float64')
+        dset = f.create_dataset('y', data = comap.dec, dtype='float64')
+
+    return 0
+
+def scalesim(datfiles, simfiles, outfiles, scale=1, beamfwhm=4.5, save=True):
+    """
+    Wrapper to load files and add simulated data. Scale can be arraylike or a single value
+    ***warn properly
+    """
+
+    # *** fix
+    if len(datfiles) != len(simfiles):
+        print('different number of files')
+        return 0
+
+    # if an array of scale values passed, only run io once and then add and dump
+    #  for each scale value
+    if isinstance(scale, (list, tuple, np.ndarray)):
+        for i in range(len(datfiles)):
+
+            datmap = load_map(datfiles[i])
+
+            simlummap = load_sim(simfiles[i])
+            simlummap = beam_smooth_sim(simlummap, fwhm=beamfwhm)
+
+            datmap.simmap = np.array(simlummap.map)
+
+            for j in range(len(scale)):
+                datmap.simdatmap = np.array(datmap.map + scale[j] * simlummap.map)
+
+                # rename the output files to have the correct scale in them
+                outfiles[i] = outfiles[i].split('_scale')[0] + '_scale'+str(j)+'.h5'
+
+                if save:
+                    dump_map(datmap, outfiles[i])
+
+    # otherwise, just run through everything once
+    else:
+        for i in range(len(datfiles)):
+
+            datmap = load_map(datfiles[i])
+
+            simlummap = load_sim(simfiles[i])
+            simlummap = beam_smooth_sim(simlummap, fwhm=beamfwhm)
+
+            datmap.simmap = np.array(simlummap.map)
+
+            datmap.simdatmap = np.array(datmap.map + scale * simlummap.map)
+
+            if save:
+                dump_map(datmap, outfiles[i])
+    return datmap
